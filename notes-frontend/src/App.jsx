5d727e91f8291764a370667f97858c42d5e20661
Aaron <vvandrine@gmail.com>
2026-07-02 20:09:49 +0530

    feat: track notes with useState and display note count

--- notes-frontend/src/App.jsx
+++ notes-frontend/src/App.jsx
@@ -1,19 +1,27 @@
-const notes = [
+import { useState } from "react"
+
+
+
+function App() {
+    const [notes,setNotes] = useState([
     {id:1, title:'Note 1', body:'This is note 1'},
     {id:2, title:'Note 2', body:'This is note 2'},
     {id:3, title:'Note 3', body:'This is note 3'}
-]
-function App() {
+])
+
   return (
     <div>
       <h1>My Notes App</h1>
+      <p>You have {notes.length} notes</p>
     <div>
         {notes.map((note) =>(
             <div key={note.id} style={{border: '1px solid black', margin: '10px', padding: '10px'}}>
                 <h2>{note.title}</h2>
                 <p>{note.body}</p>
             </div>
+            
         ))}
+        
     </div>
     </div>
 
